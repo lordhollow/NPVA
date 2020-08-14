e477f283b339b8d209d9784cf0cc1b494f6b6b94
lordhollow <hollowscircus@gmail.com>
2020-08-14 10:26:17 +0900

    部位別PV解析のリファクタリング

--- npva/PartialPVAnalyzer.cs
+++ npva/PartialPVAnalyzer.cs
@@ -36,10 +36,21 @@ namespace npva
         /// </summary>
         /// <param name="t"></param>
         /// <param name="listView"></param>
-        public virtual void Analyze(DB.Title t, ListView listView)
+        public void Analyze(DB.Title t, ListView listView)
         {
             listView.Items.Clear();
             listView.Columns.Clear();
+            analyze_execute(t, listView);
+        }
+
+        /// <summary>
+        /// 解析結果をリストビューに入れる(入れる部分)
+        /// </summary>
+        /// <param name="t"></param>
+        /// <param name="listView"></param>
+        protected virtual void analyze_execute(DB.Title t, ListView listView)
+        {
+
         }
     }
 
@@ -50,7 +61,7 @@ namespace npva
     class PPVA_PartialTotal : PartialPVAnalyzer
     {
 
-        public override void Analyze(Title t, ListView listView)
+        protected override void analyze_execute(Title t, ListView listView)
         {
             var pv = new Dictionary<int, int>();
             foreach (var score in t.Score.Where(x => x.PartPvChecked))
@@ -126,55 +137,26 @@ namespace npva
             range = n;
         }
 
-        public override void Analyze(Title t, ListView listView)
+        protected override void analyze_execute(Title t, ListView listView)
         {
             //総和数（TODO：：途中で挿話したり削除したらどうなるかはわからんです）
             var ms = t.LatestScore.Series;
-            //解析
+            
+            //全キーおよびカウンタ辞書
+            var keys = getKeys(t);
             var ppvd = new Dictionary<string, int[]>();
-            foreach (var score in t.Score.Where(x => x.PartPvChecked))
-            {
-                //scoreがrange以内か判定する
-                if (!checkRange(score.Date)) continue;
-                var mkey = DateToKey(score.Date);
-                if (ppvd.ContainsKey(mkey) == false)
-                {
-                    ppvd[mkey] = new int[ms + 1];
-                }
-                var pd = ppvd[mkey];
-                var tpv = 0;
-                foreach (var ppv in score.PartPv)
-                {
-                    tpv += ppv.PageView;
-                    if (ppv.Part <= ms)
-                    {
-                        pd[ppv.Part] += ppv.PageView;
-                    }
-                }
-                //部位別総計とその日のユニークの誤差＝目次のPV?なんか計算合わない（マイナスになるときがある）
-                pd[0] = score.UniquePageView - tpv; 
-            }
-            //表示
-            listView.Items.Clear();
-            listView.Columns.Clear();
-            listView.Columns.Add("部分");
-            //桁設定
-            var keys = new List<string>();
-            var firstDate = t.FirstUp.Date;
-            if (range > 0)
-            {
-                firstDate = DateTime.Now.AddMonths(-range + 1);
-            }
-            firstDate = firstDate.AddDays(-firstDate.Day + 1);
-            while (firstDate <= DateTime.Now.Date)
-            {
-                var k = DateToKey(firstDate);
-                keys.Add(k);
-                listView.Columns.Add(k);
-                firstDate = firstDate.AddMonths(1);
-            }
+            keys.ForEach(key => ppvd[key] = new int[ms + 1]);
+ 
+            //解析
+            countupPPV(t, ppvd);
+            
             //累積化
             if (sumup) ResolveSumup(ppvd, keys, ms);
+
+            //桁設定
+            listView.Columns.Add("部分");
+
+            keys.ForEach(x => listView.Columns.Add(x));
 
             //行設定(S=0は除外)
             for (var s = 1; s < ms + 1; s++)
@@ -195,17 +177,31 @@ namespace npva
         }
 
         /// <summary>
-        /// 日付が今からrangeか月以内か判断する。
+        /// 部分別を日付キーの辞書に話数の配列を入れて返す
         /// </summary>
-        /// <param name="d"></param>
-        /// <returns></returns>
-        bool checkRange(DateTime d)
+        /// <param name="t">作品</param>
+        /// <param name="ms">話数</param>
+        /// <returns>カウント結果</returns>
+        private void countupPPV(Title t, Dictionary<string, int[]> ppvd)
         {
-            if (range <= 0) return true;
-            var n = DateTime.Now;
-            var nM = n.Year * 12 + n.Month; //今月は何か月目？
-            var dM = d.Year * 12 + d.Month; //dは何か月目？
-            return (dM + range > nM);
+            foreach (var score in t.Score.Where(x => x.PartPvChecked))
+            {
+                //scoreがrange以内か判定する
+                if (score.Date < getFirstKeyDate(t)) continue;
+                var mkey = DateToKey(score.Date);
+                var pd = ppvd[mkey];
+                var tpv = 0;
+                foreach (var ppv in score.PartPv)
+                {
+                    tpv += ppv.PageView;
+                    if (ppv.Part < pd.Length)
+                    {
+                        pd[ppv.Part] += ppv.PageView;
+                    }
+                }
+                //部位別総計とその日のユニークの誤差＝目次のPV?なんか計算合わない（マイナスになるときがある）
+                pd[0] = score.UniquePageView - tpv;
+            }
         }
 
         /// <summary>
@@ -213,33 +209,68 @@ namespace npva
         /// </summary>
         /// <param name="d"></param>
         /// <returns></returns>
-        string DateToKey(DateTime d)
+        private string DateToKey(DateTime d)
         {
             return d.ToString("yyyy/MM");
+        }
+
+        /// <summary>
+        /// 最初のキーになる日付を取得
+        /// </summary>
+        /// <param name="t"></param>
+        /// <returns></returns>
+        private DateTime getFirstKeyDate(Title t)
+        {
+            var firstDate = t.FirstUp.Date;
+            if (range > 0)
+            {
+                firstDate = DateTime.Now.AddMonths(-range + 1);
+            }
+            firstDate = firstDate.AddDays(-firstDate.Day + 1);
+            return firstDate;
+        }
+
+        /// <summary>
+        /// 次のキーになる日付を取得
+        /// </summary>
+        /// <param name="firstDate"></param>
+        /// <returns></returns>
+        private DateTime getNextKeyDate(DateTime firstDate)
+        {
+            firstDate = firstDate.AddMonths(1);
+            return firstDate;
+        }
+
+        /// <summary>
+        /// 表示期間のキーリスト作成
+        /// </summary>
+        /// <param name="t"></param>
+        /// <returns></returns>
+        private List<string> getKeys(Title t)
+        {
+            var keys = new List<string>();
+            DateTime date = getFirstKeyDate(t);
+            while (date <= DateTime.Now.Date)
+            {
+                var k = DateToKey(date);
+                keys.Add(k);
+                date = getNextKeyDate(date);
+            }
+            return keys;
         }
 
         /// <summary>
         /// 加算解決
         /// </summary>
         /// <param name="ppvd"></param>
-        void ResolveSumup(Dictionary<string, int[]> ppvd, List<string> keys, int ms)
+        private void ResolveSumup(Dictionary<string, int[]> ppvd, List<string> keys, int ms)
         {
             //2キー目以降から、直前のキーの値を加算していく。
             for (int i = 1; i < keys.Count; i++)
             {
-                int[] pd;
-                int[] cd;
-                if (!ppvd.TryGetValue(keys[i - 1], out pd))
-                {
-                    pd = new int[ms + 1];
-                    ppvd[keys[i - 1]] = pd;
-                }
-                if (!ppvd.TryGetValue(keys[i], out cd))
-                {
-                    cd = new int[ms + 1];
-                    ppvd[keys[i]] = cd;
-                }
-                
+                int[] pd = ppvd[keys[i - 1]];
+                int[] cd = ppvd[keys[i]];
+
                 //※pdとcdのindexの数は同じであるという前提があります
 
                 for (var y = 0; y < pd.Length; y++)
