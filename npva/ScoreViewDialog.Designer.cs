abcd4139239fdcb9dfa3b879ceeedf7256928d6c
lordhollow <hollowscircus@gmail.com>
2021-02-02 17:57:17 +0900

    Eventを編集するためのダイアログを追加

---
+++ npva/ScoreViewDialog.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace npva
+{
+    partial class ScoreViewDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.txtEvent = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(302, 122);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 0;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(221, 122);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 1;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // textBox1
+            // 
+            this.txtEvent.Location = new System.Drawing.Point(12, 97);
+            this.txtEvent.Name = "textBox1";
+            this.txtEvent.Size = new System.Drawing.Size(365, 19);
+            this.txtEvent.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "イベント";
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Location = new System.Drawing.Point(16, 9);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(36, 12);
+            this.lblInfo.TabIndex = 4;
+            this.lblInfo.Text = "lblInfo";
+            // 
+            // ScoreViewDialog
+            // 
+            this.AcceptButton = this.btnOK;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnCancel;
+            this.ClientSize = new System.Drawing.Size(389, 157);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtEvent);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOK);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "ScoreViewDialog";
+            this.Text = "Score";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.TextBox txtEvent;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblInfo;
+    }
+}
---
+++ npva/ScoreViewDialog.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace npva
+{
+    public partial class ScoreViewDialog : Form
+    {
+        public ScoreViewDialog()
+        {
+            InitializeComponent();
+        }
+
+        public string EventString { get { return txtEvent.Text; } }
+
+        public void Set(DB.Author author, DB.Title title, DB.DailyScore daily)
+        {
+            var s = new StringBuilder();
+            if (author != null)
+            {
+                s.AppendLine($"{author.Name}[{author.ID}]");
+            }
+            if (title != null)
+            {
+                s.AppendLine($"{title.Name}");
+                s.AppendLine($"{title.ID} since {title.FirstUp:yyyy/MM/dd}");
+            }
+            if (daily!=null)
+            {
+                Text = daily.Date.ToString("yyyy/MM/dd");
+                s.AppendLine($"{daily.PageView} pv.");
+                if (daily.HasScoreInfo)
+                {
+                }
+                if (daily.PartPvChecked)
+                {
+                    s.AppendLine("[部位別PV取得済]");
+                }
+                txtEvent.Text = daily.Event;
+            }
+            lblInfo.Text = s.ToString();
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.OK;
+            Close();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+            Close();
+        }
+    }
+}
